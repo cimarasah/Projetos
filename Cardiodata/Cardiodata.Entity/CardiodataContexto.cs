d66d1a531ae4c3c2ca35f90cfeed210b121fc3a2
cimarasah <cimarasah@gmail.com>
2016-04-18 03:06:12 -0300

    Criação da Arquitetura

---
+++ Cardiodata/Cardiodata.Entity/CardiodataContexto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Cardiodata.Modelo;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cardiodata.Entity
+{
+    public class CardiodataContexto : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var mapPaciente = modelBuilder.Entity<Paciente>();
+            mapPaciente.Property(x => x.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            mapPaciente.Property(x => x.Nome)
+                .IsVariableLength()
+                .IsRequired()
+                .HasMaxLength(150);
+
+        }
+
+        public System.Data.Entity.DbSet<Cardiodata.Modelo.Paciente> Pacientes { get; set; }
+    }
+}
---
+++ Cardiodata/Cardiodata.Modelo/Paciente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cardiodata.Modelo
+{
+    public class Paciente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string RG { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string NomeMae { get; set; }
+        
+    }
+}
