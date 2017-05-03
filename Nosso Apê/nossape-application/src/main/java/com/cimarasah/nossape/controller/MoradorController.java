7558d100e5b4168fd3ebecae18fb809e9996f1d4
cimarasah <cimarasah@gmail.com>
2017-05-03 01:39:42 -0300

    correção application

--- Nosso Apê/nossape-application/src/main/java/com/cimarasah/nossape/controller/MoradorController.java
+++ Nosso Apê/nossape-application/src/main/java/com/cimarasah/nossape/controller/MoradorController.java
@@ -1,24 +1,24 @@
 package com.cimarasah.nossape.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cimarasah.nossape.dto.MoradorDTO;
-import com.cimarasah.nossape.service.IMoradorService;
+//import com.cimarasah.nossape.dto.MoradorDTO;
+//import com.cimarasah.nossape.service.IMoradorService;
 
 @RestController
 public class MoradorController {
 	
-	@Autowired
-	private IMoradorService moradorService;
+//	@Autowired
+//	private IMoradorService moradorService;
 
 	@RequestMapping(method=RequestMethod.GET, value="/moradores")
 	public void buscarMoradores(){
-		List<MoradorDTO> listaMoradores = moradorService.listarTodos();
-		System.out.println(listaMoradores.get(0).getNome());
+//		List<MoradorDTO> listaMoradores = moradorService.listarTodos();
+//		System.out.println(listaMoradores.get(0).getNome());
 	}
 }
